82a068fdf3610af9ecc38d60a83cf1f7753a8090
Jonathan Ronquillo <jonathanronquillo1012@gmail.com>
2022-10-13 22:51:15 +0800

    updates the views.py

--- djanghost/views.py
+++ djanghost/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello Pilipinas! \r Bagong Pilipinas Bagong Mukha !!!")
+    return HttpResponse("Hello Pilipinas! \r Bagong Pilipinas Bagong Mukha!!!")
